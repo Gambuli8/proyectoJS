87cac138d98b58ee22b7fc7394bc44473f4d6b32
gero gambuli <gerogambuli2002@gmail.com>
2022-07-05 18:39:03 -0300

    proyectojs

---
+++ index.js
@@ -0,0 +1,32 @@
+
+let nombre_usuario = prompt("ingresar nombre");
+alert("el nombre ingresado es " + nombre_usuario);
+
+let apellido_usuario = prompt("ingresar apellido");
+alert("el apellido ingresado es " + apellido_usuario);
+
+for (let i = 16; i <= 18; i++) {
+
+    let EDAD_usuario = prompt("ingrese edad");
+
+    EDAD_usuario = parseInt(EDAD_usuario);
+
+        if (EDAD_usuario >= 18) {
+        alert("SOS MAYOR DE EDAD, PUEDE SEGUIR: ", EDAD_usuario);
+            break;
+    } 
+    
+        else if (EDAD_usuario < 18); {
+        alert("SOS MENOR DE EDAD, NO PUEDE SEGUIR: ", EDAD_usuario);
+        continue;
+    } 
+}
+
+let DNI_usuario = prompt("ingresar DNI");
+alert("el DNI ingresado es " + DNI_usuario);
+
+let nacimiento_usuario = prompt("ingresar fecha de nacimiento");
+alert("la fecha de nacimiento ingresada es " + nacimiento_usuario);
+
+let EMAIL_usuario = prompt("ingresar email");
+alert("el email ingresado es " + EMAIL_usuario);
